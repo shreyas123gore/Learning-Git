59dd4a4dbdcb41c793fe812ccb992ed88c189113
Shreyas <shreyasgore2002@gmail.com>
2024-09-15 19:53:49 +0530

    Added Division.java in the dev branch

---
+++ Division.java
@@ -0,0 +1,6 @@
+class Division 
+{
+    public static void main(String[] args) {
+        System.out.println(12/6);
+    }
+}
---
+++ Subtraction.java
@@ -0,0 +1,6 @@
+public class Subtraction
+{
+    public static void main(String[] args) {
+        System.out.println(19-7);
+    }
+}
